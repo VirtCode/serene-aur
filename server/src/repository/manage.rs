e3e913752d61ac505c13ce3ed7fce9c2c93be514
WhySoBad <49595640+WhySoBad@users.noreply.github.com>
2024-07-04 22:08:45 +0200

    fix: re-sign repositry on remove, sign all repository files (#7)

--- server/src/repository/manage.rs
+++ server/src/repository/manage.rs
@@ -1,4 +1,5 @@
-use std::path::Path;
+use std::os::unix;
+use std::path::{Path, PathBuf};
 use anyhow::{anyhow, Context};
 use tokio::process::Command;
 use crate::repository::crypto;
@@ -7,21 +8,50 @@ fn db_file(name: &str) -> String {
     format!("{name}.db.tar.gz")
 }
 
+fn old_path(path: &Path) -> PathBuf {
+    path.with_file_name(format!("{}.old", path.file_name().unwrap_or_default().to_str().unwrap_or_default()))
+}
+
+fn sig_path(path: &Path) -> PathBuf {
+    path.with_file_name(format!("{}.sig", path.file_name().unwrap_or_default().to_str().unwrap_or_default()))
+}
+
+fn sign_repository(name: &str, dir: &Path) -> anyhow::Result<()> {
+    let db_path = &dir.join(format!("{name}.db"));
+    let db_archive_path = &dir.join(db_file(name));
+    let files_path = &dir.join(format!("{name}.files"));
+    let files_archive_path = &dir.join(format!("{name}.files.tar.gz"));
+
+    if old_path(db_archive_path).exists() {
+        crypto::sign(&sig_path(&old_path(db_archive_path)), &old_path(db_archive_path)).context("failed to sign old repository database")?;
+    }
+    if old_path(files_archive_path).exists() {
+        crypto::sign(&sig_path(&old_path(files_archive_path)), &old_path(files_archive_path)).context("failed to sign old repository files")?;
+    }
+
+    crypto::sign(&sig_path(db_archive_path), db_archive_path).context("failed to sign repository database")?;
+    if !sig_path(db_path).exists() {
+        unix::fs::symlink(&sig_path(db_archive_path), &sig_path(db_path)).context("failed to link repository database signature")?;
+    }
+    crypto::sign(&sig_path(files_archive_path), files_archive_path).context("failed to sign repository files")?;
+    if !sig_path(files_path).exists() {
+        unix::fs::symlink(&sig_path(files_archive_path), &sig_path(files_path)).context("failed to link repository database signature")?;
+    }
+
+    Ok(())
+}
+
 pub async fn add(name: &str, packages: &Vec<String>, dir: &Path) -> anyhow::Result<()> {
     let mut command = Command::new("repo-add");
 
     command.arg(db_file(name));
-    for package in packages {
-        command.arg(package);
-    }
+    command.args(packages);
 
     let output = command.current_dir(dir).output().await?;
 
     if output.status.success() { 
         if crypto::should_sign_packages() {
-            let signed_db_path = dir.join(format!("{}.sig", db_file(name)));
-            let db_path = dir.join(db_file(name));
-            crypto::sign(&signed_db_path, &db_path).context("failed to sign repository database")?;
+            sign_repository(name, dir)?;
         }
         Ok(())
     }
@@ -32,21 +62,23 @@ pub async fn remove(name: &str, packages: &Vec<String>, dir: &Path) -> anyhow::R
     let mut command = Command::new("repo-remove");
 
     command.arg(db_file(name));
-    for package in packages {
-        command.arg(package);
-    }
+    command.args(packages);
 
     let output = command.current_dir(dir).output().await?;
 
-    if output.status.success() { Ok(()) }
+    if output.status.success() {
+        if crypto::should_sign_packages() {
+            sign_repository(name, dir)?;
+        }
+        Ok(())
+    }
     else { Err(anyhow!("failed to use repo-remove: {}", String::from_utf8_lossy(&output.stderr))) }
 }
 
 pub async fn sign(files: &Vec<String>, base_path: &Path) -> anyhow::Result<()> {
     for file in files {
-        let path = base_path.join(file);
-        let signature_path = base_path.join(format!("{file}.sig"));
-        crypto::sign(&signature_path, &path).context(format!("failed to create signature for file: {file}"))?;
+        let path = &base_path.join(file);
+        crypto::sign(&sig_path(path), path).context(format!("failed to create signature for file: {file}"))?;
     }
     Ok(())
 }
